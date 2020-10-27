b863735ba0f5bc83f82a5577f10cecdfd89767be
Ritesh Ramkhelawon <ritesh.ramkhelawon@outlook.com>
2020-10-27 10:29:11 +0000

    chore: added eslint

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-var express = require('express');
-var exphbs  = require('express-handlebars');
- 
-var app = express();
- 
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const app = express();
+
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
- 
-app.get('/', function (req, res) {
-    res.render('home');
+
+app.get('/', (req, res) => {
+  res.render('home');
 });
- 
-app.listen(3000, function(){
-    console.log("Listening on port 3000...");
+
+app.listen(3000, () => {
+  /* eslint-disable no-console */
+  console.log('Listening on port 3000...');
+  /* eslint-enable no-console */
 });
