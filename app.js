7b20004ef5059c1d3457615cf92b98feb0b4bd4a
Ritesh Ramkhelawon <ritesh.ramkhelawon@outlook.com>
2020-10-27 10:18:51 +0000

    chore: basic handlebars app template

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var exphbs  = require('express-handlebars');
+ 
+var app = express();
+ 
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+ 
+app.get('/', function (req, res) {
+    res.render('home');
+});
+ 
+app.listen(3000, function(){
+    console.log("Listening on port 3000...");
+});
