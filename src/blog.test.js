40f9cb8a78acf916931b4bb61b839a8bd313a8aa
James Sandford <james.sandford@ecs-digital.co.uk>
2020-10-27 16:52:02 +0000

    feat: adding test for file loading

--- src/blog.test.js
+++ src/blog.test.js
@@ -4,5 +4,6 @@ const { Blog } = require('./blog');
 
 test('Loads a file correctly', async () => {
   const blog = new Blog(path.join(__dirname, '../test-data.txt').toString());
-  await blog.loadBlog().then(expect(blog.Body).toBe('this is text data'));
+  await blog.loadBlog();
+  expect(blog.Body).toBe('this is text data');
 });
