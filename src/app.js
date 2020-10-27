820b91333eb1cb252db5117c60fda20eb2d1c752
Ritesh Ramkhelawon <ritesh.ramkhelawon@outlook.com>
2020-10-27 14:31:33 +0000

    chore: fixed views filepath

--- src/app.js
+++ src/app.js
@@ -1,25 +1,18 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
-
 const app = express();
-
 const hbs = exphbs.create({
-
   extname: 'handlebars',
   layoutsDir: './src/views/layouts',
   defaultLayout: 'main',
 });
 app.set('views', `${__dirname}/views`);
 app.engine('handlebars', hbs.engine);
-
 app.set('view engine', 'handlebars');
-
 app.set('views', './src/views');
-
 app.get('/', (req, res) => {
   res.render('./src/views/home');
 });
-
 app.listen(3000, () => {
   /* eslint-disable no-console */
   console.log('Listening on port 3000...');
