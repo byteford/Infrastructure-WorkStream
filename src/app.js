cfc568776873facfec0545fb4b2e5bf53a6d3fe2
Ritesh Ramkhelawon <ritesh.ramkhelawon@outlook.com>
2020-10-27 14:16:24 +0000

    Committing code to resolve merge conflicts

--- src/app.js
+++ src/app.js
@@ -3,11 +3,19 @@ const exphbs = require('express-handlebars');
 
 const app = express();
 
+const hbs = exphbs.create({
+  extname      :'hbs',
+  layoutsDir   : './src/views/layouts',
+  defaultLayout: 'main'
+});
+
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
+app.set('views', './src/views');
+
 app.get('/', (req, res) => {
-  res.render('home');
+  res.render('./src/views/home');
 });
 
 app.listen(3000, () => {
