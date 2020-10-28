90fd6e78ac7489c0d5c8d32f92032cf5e1b217a0
Ritesh Ramkhelawon <ritesh.ramkhelawon@outlook.com>
2020-10-28 15:17:43 +0000

    feat: saves blognames in separate txt file

--- pre-build-script.sh
+++ pre-build-script.sh
@@ -2,9 +2,11 @@ mkdir -p blogs
 cd blogs
 
 input="../blog_urls.txt"
+
+blognames_file
 while IFS= read -r line
 do
   blogname=$(echo ${line##*/}|cut -f1 -d".")
-  echo ${blogname}
   curl ${line} --output ${blogname}.txt
+  echo ${blogname} >> blognames.txt
 done < "$input"
