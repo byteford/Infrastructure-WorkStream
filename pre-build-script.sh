d8faf9a95c75ca77c58fb526919994e43c712b8d
Ritesh Ramkhelawon <ritesh.ramkhelawon@outlook.com>
2020-10-29 11:27:24 +0000

    feat: size and name validation

--- pre-build-script.sh
+++ pre-build-script.sh
@@ -4,20 +4,20 @@ cd blogs
 input="../blog_urls.txt"
 > blognames.txt
 
-# while IFS= read -r line
-# do
-#   blogname=$(echo ${line##*/}|cut -f1 -d".")
-#   curl ${line} --output ${blogname}.txt
-#   echo ${blogname} >> blognames.txt
-# done < "$input"
-
 while IFS= read -r line
 do
-  size=$(curl -sI ${line} | awk '/Content-Length/{gsub("\\r", ""); print $2}')
+  expectedSize=$(curl -sI ${line} | awk '/Content-Length/{gsub("\\r", ""); print $2}')
+  blogname=$(echo ${line##*/}|cut -f1 -d".")
 
-  if [ ${size} -lt 5000 ]; then
-    blogname=$(echo ${line##*/}|cut -f1 -d".")
-    curl ${line} --output ${blogname}.txt
+  curl ${line} --output ${blogname}.txt
+
+  #get the actual file size
+  filesize=$(wc -c < ${blogname}.txt)
+  echo ${filesize}
+  echo ${expectedSize}
+  if [[ ${expectedSize} -eq ${filesize} ]] && [[ ${blogname} =~ ^[A-Za-z_]+$ ]]
+   then
+    echo "${blogname} passed"
     echo ${blogname} >> blognames.txt
   fi
 done < "$input"
