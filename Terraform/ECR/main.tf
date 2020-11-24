b5e7c5b62edcc7a1cb4007eb4b01ea1d89d782fd
James Sandford <james.sandford@ecs-digital.co.uk>
2020-11-24 09:15:53 +0000

    feat: seting up ECR module

---
+++ Terraform/ECR/main.tf
@@ -0,0 +1,10 @@
+resource "aws_ecr_repository" "main" {
+  name = var.name
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+  tags = {
+    "Owner" = var.owner
+  }
+}
--- Terraform/main.tf
+++ Terraform/main.tf
@@ -2,27 +2,8 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "HelloWorld"
-  }
+module "ECR" {
+  source = "./ECR"
+  name   = "James-Nick"
+  owner  = "James-Nick"
 }
