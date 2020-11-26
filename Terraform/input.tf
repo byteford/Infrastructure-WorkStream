58dafbcbb536d9879855adcb7e6f85e5c205c84c
James Sandford <james.sandford@ecs-digital.co.uk>
2020-11-26 16:32:36 +0000

    more formatting

--- Terraform/input.tf
+++ Terraform/input.tf
@@ -7,6 +7,6 @@ variable "owner" {
   default = "James-Nick"
 }
 variable "ECR_REPO" {
-  type    = string
+  type = string
   #default = "603825719481.dkr.ecr.eu-west-2.amazonaws.com/james-nick:9608de324850e23f4341e3318280c542a07ff75e"
 }
