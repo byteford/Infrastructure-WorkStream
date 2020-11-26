8ed753b55a6d800fa0e202ecf7618606c0ea50b8
James Sandford <james.sandford@ecs-digital.co.uk>
2020-11-26 09:59:36 +0000

    reenable the task

--- Terraform/ECS/main.tf
+++ Terraform/ECS/main.tf
@@ -1,11 +1,9 @@
 resource "aws_ecs_service" "web" {
   name    = "web"
   cluster = aws_ecs_cluster.main.id
-  #task_definition = aws_ecs_task_definition.webService.arn
+  task_definition = aws_ecs_task_definition.webService.arn
   desired_count = 1
-  #iam_role        = aws_iam_role.foo.arn
-  #depends_on      = [aws_iam_role_policy.foo]
-  launch_type = "FARGATE"
+  launch_type   = "FARGATE"
   load_balancer {
     target_group_arn = var.lbTarget
     container_name   = var.name
