4ad9ad9f0a63b6c3007567dc0a74f20413e7b84d
James Sandford <james.sandford@ecs-digital.co.uk>
2020-11-26 16:49:12 +0000

    changing to latest

--- Terraform/input.tf
+++ Terraform/input.tf
@@ -8,5 +8,5 @@ variable "owner" {
 }
 variable "ECR_REPO" {
   type = string
-  #default = "603825719481.dkr.ecr.eu-west-2.amazonaws.com/james-nick:9608de324850e23f4341e3318280c542a07ff75e"
+  default = "603825719481.dkr.ecr.eu-west-2.amazonaws.com/james-nick:latest"
 }
