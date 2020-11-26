5529b2d3a61d22b91018ee3c9d9df6d57d3cfe6d
James Sandford <james.sandford@ecs-digital.co.uk>
2020-11-26 10:22:54 +0000

    corrected the predefined metric

--- Terraform/ECS/scaling.tf
+++ Terraform/ECS/scaling.tf
@@ -8,12 +8,13 @@ resource "aws_appautoscaling_target" "ecs_target" {
 resource "aws_appautoscaling_policy" "ecs_policy" {
   name               = var.name
   service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
-  policy_type        = "TargetTrackingScaling"
+  scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
   resource_id        = aws_appautoscaling_target.ecs_target.resource_id
+  policy_type        = "TargetTrackingScaling"
 
   target_tracking_scaling_policy_configuration {
     predefined_metric_specification {
-      predefined_metric_type = "RDSReaderAverageCPUUtilization"
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
     }
     disable_scale_in   = true
     target_value       = 4
