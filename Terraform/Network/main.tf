9f9a104fa93fc107bf37b6db6ae8dc93f8d3c035
James Sandford <james.sandford@ecs-digital.co.uk>
2020-11-26 13:00:42 +0000

    removed public ip

--- Terraform/Network/main.tf
+++ Terraform/Network/main.tf
@@ -1,9 +1,9 @@
 resource "aws_subnet" "public" {
-  count                   = 2
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.${count.index + 2}.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = element(var.availability_zones.*, count.index % length(var.availability_zones))
+  count      = 2
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.${count.index + 2}.0/24"
+  #map_public_ip_on_launch = true
+  availability_zone = element(var.availability_zones.*, count.index % length(var.availability_zones))
 
   tags = {
     Name  = var.name
--- Terraform/output.tf
+++ Terraform/output.tf
@@ -1,3 +1,6 @@
 output "ECR_repo_url" {
   value = module.ECR.repo_url
 }
+output "DNSName" {
+  value = module.Network.loadbalancer.dns_name
+}
